2b0db22609dbccfbd67829197f2eb913a28544d8
Shawn Stephens <shawnstephens517@gmail.com>
2022-11-21 10:54:51 -0500

    add repos and build badge. Update actions.

--- syncfile.sh
+++ syncfile.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-reposync -g -l -d -m -q --repoid=centosplus --download-metadata --newest-only
-reposync -g -l -d -m -q --repoid=extras --download-metadata --newest-only
-reposync -g -l -d -m -q --repoid=updates --download-metadata --newest-only
-reposync -g -l -d -m -q --repoid=epel --download-metadata --newest-only
+reposync -g -l -d -m --repoid=centosplus --download-metadata --newest-only
+reposync -g -l -d -m --repoid=extras --download-metadata --newest-only
+reposync -g -l -d -m --repoid=updates --download-metadata --newest-only
+reposync -g -l -d -m --repoid=fasttrack --download-metadata --newest-only
+
+reposync -g -l -d -m --repoid=elrepo --newest-only --download-metadata
+reposync -g -l -d -m --repoid=elrepo-extras --newest-only --download-metadata 
+reposync -g -l -d -m --repoid=elrepo-kernel --newest-only --download-metadata
+reposync -g -l -d -m --repoid=elrepo-testing --newest-only --download-metadata
+
+reposync -g -l -d -m --repoid=epel --download-metadata --newest-only
+reposync -g -l -d -m --repoid=epel-debuginfo --newest-only --download-metadata 
+reposync -g -l -d -m --repoid=epel-testing --newest-only --download-metadata 
+reposync -g -l -d -m --repoid=epel-testing-debuginfo --newest-only --download-metadata 
+#reposync -g -l -d -m --repoid=epel-extras --newest-only --download-metadata 
+
+reposync -g -l -d -m --repoid=pgdg96 --newest-only --download-metadata 
 
 #Create ISO of your updated repos
 FILE=./repos.iso
@@ -13,6 +26,6 @@ if [ -e "$FILE" ]; then
     read -t 3
 else 
     echo "Creating repos.tgz"
-    find * -type f -daystart -mtime -1 -exec tar -czf "repos_$(date '+%m-%d-%Y').tgz" "{}" + | split -b 4092M - "repos_$(date '+%m-%d-%Y').tgz.part"
+    tar -czf repos.tgz centosplus/ extras/ updates/ epel/ | split -b 4092M - "repos.tgz.part"
 fi
 
