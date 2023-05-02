ec329efead491ec276e4c8a7df5b350e37b90ef3
Shawn Stephens <shawnstephens517@gmail.com>
2023-05-02 09:37:00 -0400

    Update Sync
    Trigger new container build

--- syncfile.sh
+++ syncfile.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+####
+##Download Base Packages##
+#####
+reposync -g -l -d -m --repoid=base --newest-only --download-metadata
 reposync -g -l -d -m --repoid=centosplus --download-metadata --newest-only
 reposync -g -l -d -m --repoid=extras --download-metadata --newest-only
 reposync -g -l -d -m --repoid=updates --download-metadata --newest-only
 reposync -g -l -d -m --repoid=fasttrack --download-metadata --newest-only
 
+####
+##Download Extra Packages ##
+#####
 reposync -g -l -d -m --repoid=elrepo --newest-only --download-metadata
 reposync -g -l -d -m --repoid=elrepo-extras --newest-only --download-metadata 
 reposync -g -l -d -m --repoid=elrepo-kernel --newest-only --download-metadata
@@ -15,7 +22,6 @@ reposync -g -l -d -m --repoid=epel --download-metadata --newest-only
 #reposync -g -l -d -m --repoid=epel-testing-debuginfo --newest-only --download-metadata 
 reposync -g -l -d -m --repoid=epel-extras --newest-only --download-metadata
 reposync -g -l -d -m --repoid=os --newest-only --download-metadata
-reposync -g -l -d -m --repoid=base --newest-only --download-metadata
 
 #reposync -g -l -d -m --repoid=pgdg96 --newest-only --download-metadata 
 
